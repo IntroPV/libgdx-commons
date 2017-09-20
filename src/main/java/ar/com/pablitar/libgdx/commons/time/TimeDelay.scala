c1b7bd1f0012f982d98c36147671db2aa52134e0
pablitar <pablitar@gmail.com>
2017-09-19 22:42:36 -0300

    Agregada posibilidad de detener el tiempo durante una demora

--- src/main/java/ar/com/pablitar/libgdx/commons/time/TimeDelay.scala
+++ src/main/java/ar/com/pablitar/libgdx/commons/time/TimeDelay.scala
@@ -2,21 +2,24 @@ package ar.com.pablitar.libgdx.commons.time
 
 import com.badlogic.gdx.math.Interpolation
 
-class TimeDelay(var duration: Float, var rate: Float = 0.5f, interpolator: Interpolation = Interpolation.pow3) {
+class TimeDelay(val duration: Float, val rate: Float = 0.5f, interpolator: Interpolation = Interpolation.pow3, val stopTimeDuration: Float = 0f) {
   var elapsed: Float = duration
+  require(duration >= stopTimeDuration, "The total duration should be bigger than the duration of stop-time")
 
   def update(delta: Float) = {
-    elapsed = (elapsed + delta).min(duration) 
+    elapsed = (elapsed + delta).min(duration)
   }
-  
-  def currentRate = interpolator.apply(1 - rate, 1, elapsed / duration)
-  
+
+  def currentRate = {
+    if (elapsed < stopTimeDuration) 0 else
+      interpolator.apply(1 - rate, 1, elapsed / (duration - stopTimeDuration))
+  }
+
   def startDelaying = {
     elapsed = 0f
   }
-  
+
   def apply(delta: Float) = {
-    println(currentRate)
     delta * currentRate
   }
 }
